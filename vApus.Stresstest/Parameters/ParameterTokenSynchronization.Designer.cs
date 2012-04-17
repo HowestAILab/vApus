0377fcb7c6218450978abd4a32f6bef868df899a
Dieter <dieter@sizingservers.be>
2012-04-17 09:23:09 +0200

    Fixed #55

--- vApus.Stresstest/Parameters/ParameterTokenSynchronization.Designer.cs
+++ vApus.Stresstest/Parameters/ParameterTokenSynchronization.Designer.cs
@@ -33,6 +33,7 @@
             this.clmParameter = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.clmOld = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.clmNew = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -41,11 +42,11 @@
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(12, 14);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(633, 32);
+            this.label1.Size = new System.Drawing.Size(636, 32);
             this.label1.TabIndex = 1;
-            this.label1.Text = "Because one or more parameters where added or removed tokens used in the log entr" +
-    "ies where updated.\r\nThe numeric part of those tokens where synchronized with the" +
-    " indices of the parameters:";
+            this.label1.Text = "Because one or more parameters where added or removed, tokens used in the log ent" +
+    "ries where updated.\r\nThe numeric parts of those tokens where synchronized with t" +
+    "he indices of the parameters.";
             // 
             // lvw
             // 
@@ -57,9 +58,11 @@
             this.clmParameter,
             this.clmOld,
             this.clmNew});
-            this.lvw.Location = new System.Drawing.Point(0, 60);
+            this.lvw.FullRowSelect = true;
+            this.lvw.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.lvw.Location = new System.Drawing.Point(0, 99);
             this.lvw.Name = "lvw";
-            this.lvw.Size = new System.Drawing.Size(666, 434);
+            this.lvw.Size = new System.Drawing.Size(666, 395);
             this.lvw.TabIndex = 3;
             this.lvw.UseCompatibleStateImageBehavior = false;
             this.lvw.View = System.Windows.Forms.View.Details;
@@ -81,11 +84,26 @@
             this.clmNew.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             this.clmNew.Width = 46;
             // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(125, 60);
+            this.label2.Name = "label2";
+            this.label2.Padding = new System.Windows.Forms.Padding(3);
+            this.label2.Size = new System.Drawing.Size(432, 32);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Please double-check the log entries that contain parameter tokens.\r\nIn some cases" +
+    " they cannot be synchronized! (e.g. {1} cannot become {0})";
+            // 
             // ParameterTokenSynchronization
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(666, 494);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.lvw);
             this.Controls.Add(this.label1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -104,5 +122,6 @@
         private System.Windows.Forms.ColumnHeader clmParameter;
         private System.Windows.Forms.ColumnHeader clmOld;
         private System.Windows.Forms.ColumnHeader clmNew;
+        private System.Windows.Forms.Label label2;
     }
 }
