dba8af2a9770b286d33918450ea517f0215250e9
Dieter Vandroemme <dieter@sizingservers.be>
2013-01-30 16:56:57 +0100

    #336

---
+++ vApus.Stresstest/Connections/ConnectionsView.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace vApus.Stresstest {
+    partial class ConnectionsView {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dgvConnections = new System.Windows.Forms.DataGridView();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnDuplicate = new System.Windows.Forms.Button();
+            this.btnRemoveAll = new System.Windows.Forms.Button();
+            this.btnSort = new System.Windows.Forms.Button();
+            this.chkShowConnectionStrings = new System.Windows.Forms.CheckBox();
+            this.btnEdit = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvConnections)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvConnections
+            // 
+            this.dgvConnections.AllowUserToAddRows = false;
+            this.dgvConnections.AllowUserToDeleteRows = false;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            this.dgvConnections.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgvConnections.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvConnections.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dgvConnections.BackgroundColor = System.Drawing.Color.White;
+            this.dgvConnections.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dgvConnections.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.None;
+            this.dgvConnections.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            this.dgvConnections.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvConnections.EnableHeadersVisualStyles = false;
+            this.dgvConnections.Font = new System.Drawing.Font("Consolas", 9.75F);
+            this.dgvConnections.Location = new System.Drawing.Point(12, 42);
+            this.dgvConnections.Name = "dgvConnections";
+            this.dgvConnections.ReadOnly = true;
+            this.dgvConnections.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            this.dgvConnections.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.dgvConnections.Size = new System.Drawing.Size(612, 305);
+            this.dgvConnections.TabIndex = 7;
+            this.dgvConnections.VirtualMode = true;
+            this.dgvConnections.CellEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvConnections_CellEnter);
+            this.dgvConnections.CellFormatting += new System.Windows.Forms.DataGridViewCellFormattingEventHandler(this.dgvConnections_CellFormatting);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.AutoSize = true;
+            this.btnAdd.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnAdd.BackColor = System.Drawing.SystemColors.Control;
+            this.btnAdd.FlatAppearance.BorderColor = System.Drawing.Color.DimGray;
+            this.btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(12, 12);
+            this.btnAdd.MaximumSize = new System.Drawing.Size(165, 24);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(41, 24);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.AutoSize = true;
+            this.btnRemove.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnRemove.BackColor = System.Drawing.SystemColors.Control;
+            this.btnRemove.Enabled = false;
+            this.btnRemove.FlatAppearance.BorderColor = System.Drawing.Color.DimGray;
+            this.btnRemove.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRemove.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRemove.Location = new System.Drawing.Point(243, 12);
+            this.btnRemove.MaximumSize = new System.Drawing.Size(165, 24);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(65, 24);
+            this.btnRemove.TabIndex = 4;
+            this.btnRemove.Text = "Remove";
+            this.btnRemove.UseVisualStyleBackColor = false;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // btnDuplicate
+            // 
+            this.btnDuplicate.AutoSize = true;
+            this.btnDuplicate.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnDuplicate.BackColor = System.Drawing.SystemColors.Control;
+            this.btnDuplicate.Enabled = false;
+            this.btnDuplicate.FlatAppearance.BorderColor = System.Drawing.Color.DimGray;
+            this.btnDuplicate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDuplicate.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDuplicate.Location = new System.Drawing.Point(106, 12);
+            this.btnDuplicate.MaximumSize = new System.Drawing.Size(165, 24);
+            this.btnDuplicate.Name = "btnDuplicate";
+            this.btnDuplicate.Size = new System.Drawing.Size(73, 24);
+            this.btnDuplicate.TabIndex = 2;
+            this.btnDuplicate.Text = "Duplicate";
+            this.btnDuplicate.UseVisualStyleBackColor = false;
+            this.btnDuplicate.Click += new System.EventHandler(this.btnDuplicate_Click);
+            // 
+            // btnRemoveAll
+            // 
+            this.btnRemoveAll.AutoSize = true;
+            this.btnRemoveAll.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnRemoveAll.BackColor = System.Drawing.SystemColors.Control;
+            this.btnRemoveAll.Enabled = false;
+            this.btnRemoveAll.FlatAppearance.BorderColor = System.Drawing.Color.DimGray;
+            this.btnRemoveAll.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRemoveAll.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRemoveAll.Location = new System.Drawing.Point(314, 12);
+            this.btnRemoveAll.MaximumSize = new System.Drawing.Size(165, 24);
+            this.btnRemoveAll.Name = "btnRemoveAll";
+            this.btnRemoveAll.Size = new System.Drawing.Size(83, 24);
+            this.btnRemoveAll.TabIndex = 5;
+            this.btnRemoveAll.Text = "Remove All";
+            this.btnRemoveAll.UseVisualStyleBackColor = false;
+            this.btnRemoveAll.Click += new System.EventHandler(this.btnRemoveAll_Click);
+            // 
+            // btnSort
+            // 
+            this.btnSort.AutoSize = true;
+            this.btnSort.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnSort.BackColor = System.Drawing.SystemColors.Control;
+            this.btnSort.Enabled = false;
+            this.btnSort.FlatAppearance.BorderColor = System.Drawing.Color.DimGray;
+            this.btnSort.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSort.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSort.Location = new System.Drawing.Point(190, 12);
+            this.btnSort.MaximumSize = new System.Drawing.Size(165, 24);
+            this.btnSort.Name = "btnSort";
+            this.btnSort.Size = new System.Drawing.Size(42, 24);
+            this.btnSort.TabIndex = 3;
+            this.btnSort.Text = "Sort";
+            this.btnSort.UseVisualStyleBackColor = false;
+            this.btnSort.Click += new System.EventHandler(this.btnSort_Click);
+            // 
+            // chkShowConnectionStrings
+            // 
+            this.chkShowConnectionStrings.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.chkShowConnectionStrings.AutoSize = true;
+            this.chkShowConnectionStrings.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.chkShowConnectionStrings.Location = new System.Drawing.Point(482, 16);
+            this.chkShowConnectionStrings.Margin = new System.Windows.Forms.Padding(3, 7, 0, 3);
+            this.chkShowConnectionStrings.Name = "chkShowConnectionStrings";
+            this.chkShowConnectionStrings.Size = new System.Drawing.Size(142, 17);
+            this.chkShowConnectionStrings.TabIndex = 6;
+            this.chkShowConnectionStrings.Text = "Show Connection Strings";
+            this.chkShowConnectionStrings.UseVisualStyleBackColor = true;
+            this.chkShowConnectionStrings.CheckedChanged += new System.EventHandler(this.chkShowConnectionStrings_CheckedChanged);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.AutoSize = true;
+            this.btnEdit.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnEdit.BackColor = System.Drawing.SystemColors.Control;
+            this.btnEdit.FlatAppearance.BorderColor = System.Drawing.Color.DimGray;
+            this.btnEdit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEdit.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEdit.Location = new System.Drawing.Point(59, 12);
+            this.btnEdit.MaximumSize = new System.Drawing.Size(165, 24);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(41, 24);
+            this.btnEdit.TabIndex = 1;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = false;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // ConnectionsView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(636, 359);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnSort);
+            this.Controls.Add(this.btnRemoveAll);
+            this.Controls.Add(this.btnDuplicate);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.dgvConnections);
+            this.Controls.Add(this.chkShowConnectionStrings);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "ConnectionsView";
+            this.Text = "ConnectionsView";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvConnections)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvConnections;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Button btnDuplicate;
+        private System.Windows.Forms.Button btnRemoveAll;
+        private System.Windows.Forms.Button btnSort;
+        private System.Windows.Forms.CheckBox chkShowConnectionStrings;
+        private System.Windows.Forms.Button btnEdit;
+    }
+}
---
+++ vApus.Stresstest/Connections/ConnectionsView.cs
@@ -0,0 +1,132 @@
+﻿/*
+ * Copyright 2013 (c) Sizing Servers Lab
+ * University College of West-Flanders, Department GKG
+ * 
+ * Author(s):
+ *    Dieter Vandroemme
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Windows.Forms;
+using vApus.SolutionTree;
+using vApus.Util;
+
+namespace vApus.Stresstest {
+    public partial class ConnectionsView : BaseSolutionComponentView {
+        private readonly Connections _connections;
+        private readonly DataTable _dataSource;
+        private volatile int _preferedRowToSelect = -1;
+
+        public ConnectionsView() {
+            InitializeComponent();
+        }
+
+        public ConnectionsView(SolutionComponent solutionComponent, params object[] args)
+            : base(solutionComponent, args) {
+            InitializeComponent();
+            _connections = solutionComponent as Connections;
+            _dataSource = new DataTable("Connections");
+            _dataSource.Columns.Add("Connection");
+            _dataSource.Columns.Add("Connection Proxy");
+            _dataSource.Columns.Add("Connection String");
+
+            foreach (DataGridViewColumn clm in dgvConnections.Columns) clm.SortMode = DataGridViewColumnSortMode.NotSortable;
+
+            FillConnectionsDatagrid();
+
+            SolutionComponent.SolutionComponentChanged += SolutionComponent_SolutionComponentChanged;
+        }
+
+        private void SolutionComponent_SolutionComponentChanged(object sender, SolutionComponentChangedEventArgs e) {
+            if (sender == _connections || sender is Connection) FillConnectionsDatagrid();
+        }
+        public void FillConnectionsDatagrid() {
+            dgvConnections.DataSource = null;
+            _dataSource.Clear();
+            foreach (var item in _connections)
+                if (item is Connection) {
+                    var connection = item as Connection;
+                    var row = _dataSource.Rows.Add(connection.ToString(), connection.ConnectionProxy.ToString(), connection.ConnectionString);
+                    row.SetTag(connection);
+                }
+            dgvConnections.DataSource = _dataSource;
+
+            if (dgvConnections.RowCount == 0)
+                btnEdit.Enabled = btnDuplicate.Enabled = btnRemove.Enabled = btnRemoveAll.Enabled = btnSort.Enabled = false;
+            else {
+                dgvConnections.ClearSelection();
+                if (_preferedRowToSelect == -1)
+                    dgvConnections.Rows[0].Selected = true;
+                else
+                    try {
+                        dgvConnections.Rows[_preferedRowToSelect].Selected = true;
+                    } catch { dgvConnections.Rows[0].Selected = true; }
+            }
+        }
+        private void dgvConnections_CellFormatting(object sender, DataGridViewCellFormattingEventArgs e) {
+            if (!chkShowConnectionStrings.Checked && e.ColumnIndex == 2 && e.Value != null) {
+                e.Value = new string('*', e.Value.ToString().Length);
+            }
+        }
+        private void dgvConnections_CellEnter(object sender, DataGridViewCellEventArgs e) {
+            btnEdit.Enabled = btnDuplicate.Enabled = btnRemove.Enabled = btnRemoveAll.Enabled = btnSort.Enabled = true;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e) {
+            _preferedRowToSelect = _dataSource.Rows.Count;
+            var connection = new Connection();
+            _connections.Add(connection);
+            //connection.Activate();
+        }
+        private void btnDuplicate_Click(object sender, EventArgs e) {
+            SolutionComponent.SolutionComponentChanged -= SolutionComponent_SolutionComponentChanged;
+            _preferedRowToSelect = _dataSource.Rows.Count;
+            var connections = new List<Connection>();
+            foreach (DataGridViewCell cell in dgvConnections.SelectedCells) {
+                var connection = _dataSource.Rows[cell.RowIndex].GetTag() as Connection;
+                if (connection != null && !connections.Contains(connection)) {
+                    connections.Add(connection);
+                    connection.Duplicate();
+                }
+            }
+            SolutionComponent.SolutionComponentChanged += SolutionComponent_SolutionComponentChanged;
+            FillConnectionsDatagrid();
+        }
+        private void btnEdit_Click(object sender, EventArgs e) {
+            foreach (DataGridViewCell cell in dgvConnections.SelectedCells) {
+                dgvConnections.Rows[cell.RowIndex].Cells[0].Selected = true;
+                var connection = _dataSource.Rows[cell.RowIndex].GetTag() as Connection;
+                if (connection != null) connection.Activate();
+                break;
+            }
+        }
+        private void btnSort_Click(object sender, EventArgs e) {
+            _preferedRowToSelect = -1;
+            _connections.SortItemsByLabel();
+        }
+        private void btnRemove_Click(object sender, EventArgs e) {
+            _preferedRowToSelect = -1;
+            var connections = new List<Connection>();
+            foreach (DataGridViewCell cell in dgvConnections.SelectedCells) {
+                var connection = _dataSource.Rows[cell.RowIndex].GetTag() as Connection;
+                if (connection != null && !connections.Contains(connection)) connections.Add(connection);
+                if (_preferedRowToSelect == -1) _preferedRowToSelect = cell.RowIndex;
+            }
+
+            SolutionComponent.SolutionComponentChanged -= SolutionComponent_SolutionComponentChanged;
+            bool removed = _connections.RemoveRange(connections);
+            SolutionComponent.SolutionComponentChanged += SolutionComponent_SolutionComponentChanged;
+            if (removed) FillConnectionsDatagrid();
+        }
+        private void btnRemoveAll_Click(object sender, EventArgs e) {
+            _preferedRowToSelect = -1;
+            _connections.Clear();
+        }
+
+        private void chkShowConnectionStrings_CheckedChanged(object sender, EventArgs e) {
+            dgvConnections.Refresh();
+        }
+    }
+}
