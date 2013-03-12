6deb2304af853a633ae5be701aad5adc7897b55b
Dieter Vandroemme <dieter@sizingservers.be>
2013-03-12 14:22:50 +0100

    #318

--- vApus.Stresstest/Logs/Controls/LogEntryControl.cs
+++ vApus.Stresstest/Logs/Controls/LogEntryControl.cs
@@ -11,10 +11,8 @@ using System.Windows.Forms;
 using vApus.SolutionTree;
 using vApus.Stresstest.Properties;
 
-namespace vApus.Stresstest
-{
-    public partial class LogEntryControl : LogChildControlBase
-    {
+namespace vApus.Stresstest {
+    public partial class LogEntryControl : LogChildControlBase {
         #region Events
 
         public event EventHandler LexicalResultChanged;
@@ -43,29 +41,24 @@ namespace vApus.Stresstest
         /// <summary>
         ///     The base item that it holds, in this case a log entry.
         /// </summary>
-        public override BaseItem LogChild
-        {
+        public override BaseItem LogChild {
             get { return _logEntry; }
         }
 
-        public override uint IndentationLevel
-        {
+        public override uint IndentationLevel {
             get { return _indentationLevel; }
         }
 
-        public override bool Checked
-        {
+        public override bool Checked {
             get { return chkIndex.Checked; }
             set { chkIndex.Checked = value; }
         }
 
-        public LexicalResult LexicalResult
-        {
+        public LexicalResult LexicalResult {
             get { return _logEntry.LexicalResult; }
         }
 
-        public UserActionControl UserActionControl
-        {
+        public UserActionControl UserActionControl {
             get { return _userActionControl; }
         }
 
@@ -76,16 +69,14 @@ namespace vApus.Stresstest
         /// <summary>
         ///     An empty log entry control, aught to use only when designing.
         /// </summary>
-        public LogEntryControl()
-        {
+        public LogEntryControl() {
             InitializeComponent();
         }
 
         /// <summary>
         ///     Visualizez a log entry.
         /// </summary>
-        public LogEntryControl(LogEntry logEntry)
-        {
+        public LogEntryControl(LogEntry logEntry) {
             InitializeComponent();
             _logEntry = logEntry;
             _logEntry.LexicalResultChanged += _logEntry_LexicalResultChanged;
@@ -116,40 +107,28 @@ namespace vApus.Stresstest
 
         #region Functions
 
-        private void SolutionComponent_SolutionComponentChanged(object sender, SolutionComponentChangedEventArgs e)
-        {
-            if (sender == _logEntry
-                || (sender is Log && (sender == _logEntry.Parent
-                                      ||
-                                      (_logEntry.Parent is UserAction &&
-                                       sender == (_logEntry.Parent as UserAction).Parent))))
-            {
+        private void SolutionComponent_SolutionComponentChanged(object sender, SolutionComponentChangedEventArgs e) {
+            if (sender == _logEntry || (sender is Log && (sender == _logEntry.Parent || (_logEntry.Parent is UserAction && sender == (_logEntry.Parent as UserAction).Parent)))) {
                 SetImages();
                 SetLabel();
             }
         }
 
-        private void _logEntry_LexicalResultChanged(object sender, EventArgs e)
-        {
+        private void _logEntry_LexicalResultChanged(object sender, EventArgs e) {
             SetImages();
             if (LexicalResultChanged != null)
                 LexicalResultChanged(this, null);
         }
 
-        private void SetLabel()
-        {
-            lblLogEntry.Text = _logEntry.LogEntryString.Replace("&", "&&")
-                                        .Replace(VBLRn, _delimiterReplacement)
-                                        .Replace(VBLRr, _delimiterReplacement);
+        private void SetLabel() {
+            lblLogEntry.Text = _logEntry.LogEntryString.Replace("&", "&&").Replace(VBLRn, _delimiterReplacement).Replace(VBLRr, _delimiterReplacement);
 
             if (_logChildDelimiter != null && _logChildDelimiter.Length != 0)
                 lblLogEntry.Text = lblLogEntry.Text.Replace(_logChildDelimiter, _delimiterReplacement);
         }
 
-        private void SetImages()
-        {
-            switch (_logEntry.LexicalResult)
-            {
+        private void SetImages() {
+            switch (_logEntry.LexicalResult) {
                 case LexicalResult.OK:
                     picValidation.Image = Resources.LogEntryOK;
                     break;
@@ -164,60 +143,49 @@ namespace vApus.Stresstest
             picPin.Visible = _logEntry.Parent is Log;
         }
 
-        private void chkIndex_CheckedChanged(object sender, EventArgs e)
-        {
+        private void chkIndex_CheckedChanged(object sender, EventArgs e) {
             InvokeCheckedChanged();
         }
 
-        private void picPin_Click(object sender, EventArgs e)
-        {
+        private void picPin_Click(object sender, EventArgs e) {
             _logEntry.Pinned = !_logEntry.Pinned;
             _logEntry.InvokeSolutionComponentChangedEvent(SolutionComponentChangedEventArgs.DoneAction.Edited);
             SetImages();
         }
 
-        private void picIgnoreDelay_Click(object sender, EventArgs e)
-        {
-            if (!_logEntry.ExecuteInParallelWithPrevious)
-            {
+        private void picIgnoreDelay_Click(object sender, EventArgs e) {
+            if (!_logEntry.ExecuteInParallelWithPrevious) {
                 _logEntry.IgnoreDelay = !_logEntry.IgnoreDelay;
                 _logEntry.InvokeSolutionComponentChangedEvent(SolutionComponentChangedEventArgs.DoneAction.Edited);
                 SetImages();
             }
         }
 
-        private void picParallel_Click(object sender, EventArgs e)
-        {
+        private void picParallel_Click(object sender, EventArgs e) {
             _logEntry.ExecuteInParallelWithPrevious = !_logEntry.ExecuteInParallelWithPrevious;
             _logEntry.InvokeSolutionComponentChangedEvent(SolutionComponentChangedEventArgs.DoneAction.Edited);
             SetImages();
         }
 
-        private void nudOccurance_ValueChanged(object sender, EventArgs e)
-        {
-            if (_logEntry.Occurance != nudOccurance.Value)
-            {
-                _logEntry.Occurance = (int) nudOccurance.Value;
+        private void nudOccurance_ValueChanged(object sender, EventArgs e) {
+            if (_logEntry.Occurance != nudOccurance.Value) {
+                _logEntry.Occurance = (int)nudOccurance.Value;
                 _logEntry.InvokeSolutionComponentChangedEvent(SolutionComponentChangedEventArgs.DoneAction.Edited);
             }
         }
 
-        private void nudParallelOffsetInMs_ValueChanged(object sender, EventArgs e)
-        {
-            if (_logEntry.ParallelOffsetInMs != nudParallelOffsetInMs.Value)
-            {
-                _logEntry.ParallelOffsetInMs = (int) nudParallelOffsetInMs.Value;
+        private void nudParallelOffsetInMs_ValueChanged(object sender, EventArgs e) {
+            if (_logEntry.ParallelOffsetInMs != nudParallelOffsetInMs.Value) {
+                _logEntry.ParallelOffsetInMs = (int)nudParallelOffsetInMs.Value;
                 _logEntry.InvokeSolutionComponentChangedEvent(SolutionComponentChangedEventArgs.DoneAction.Edited);
             }
         }
 
-        private void llblEdit_Click(object sender, EventArgs e)
-        {
+        private void llblEdit_Click(object sender, EventArgs e) {
             Edit();
         }
 
-        private void toolStripStatusLabelRemove_Click(object sender, EventArgs e)
-        {
+        private void toolStripStatusLabelRemove_Click(object sender, EventArgs e) {
             if (Removed != null)
                 Removed(this, null);
         }
@@ -225,8 +193,7 @@ namespace vApus.Stresstest
         /// <summary>
         ///     Augment the indentlevel by one (or level of dependency) of the container of the control.
         /// </summary>
-        public void SetUserActionControl(UserActionControl userActionControl)
-        {
+        public void SetUserActionControl(UserActionControl userActionControl) {
             _userActionControl = userActionControl;
 
             SetImages();
@@ -241,14 +208,11 @@ namespace vApus.Stresstest
         /// <summary>
         ///     Reduce the indentlevel by one (or level of dependency) of the container of the control. If the indentlevel is smaller then zero it becomes zero.
         /// </summary>
-        public void RemoveUserActionControl()
-        {
+        public void RemoveUserActionControl() {
             _userActionControl = null;
-            if (Margin.Left != MINIMUMLEFTMARGIN)
-            {
+            if (Margin.Left != MINIMUMLEFTMARGIN) {
                 SetImages();
-                if (Margin.Left > MINIMUMLEFTMARGIN)
-                {
+                if (Margin.Left > MINIMUMLEFTMARGIN) {
                     Margin = new Padding(Margin.Left - INDENTATIONOFFSET, 3, 3, 3);
                     Width += INDENTATIONOFFSET;
                 }
@@ -263,11 +227,9 @@ namespace vApus.Stresstest
         /// <summary>
         ///     To edit the value of the container of the control.
         /// </summary>
-        public void Edit()
-        {
+        public void Edit() {
             var addEditLogEntry = new AddEditLogEntry(LogChild as LogEntry);
-            if (addEditLogEntry.ShowDialog(this) == DialogResult.OK)
-            {
+            if (addEditLogEntry.ShowDialog(this) == DialogResult.OK) {
                 SolutionComponent parent = _logEntry.Parent;
                 int index = parent.IndexOf(_logEntry);
                 parent.RemoveWithoutInvokingEvent(_logEntry);
@@ -285,13 +247,11 @@ namespace vApus.Stresstest
         /// <summary>
         ///     Clears the log entry. No events will be invoked, you should do this yourself afterwards.
         /// </summary>
-        public override void ClearLogChild()
-        {
+        public override void ClearLogChild() {
             _logEntry.ClearWithoutInvokingEvent();
         }
 
-        protected override void OnPaint(PaintEventArgs e)
-        {
+        protected override void OnPaint(PaintEventArgs e) {
             base.OnPaint(e);
             int index = _logEntry.Parent.IndexOf(_logEntry) + 1;
             chkIndex.Text = index.ToString();
@@ -305,27 +265,26 @@ namespace vApus.Stresstest
             if (!picParallel.Enabled)
                 _logEntry.ExecuteInParallelWithPrevious = false;
 
-            if (_logEntry.ExecuteInParallelWithPrevious)
-            {
+            if (_logEntry.ExecuteInParallelWithPrevious) {
                 _logEntry.IgnoreDelay = true;
                 picIgnoreDelay.Visible = false;
                 nudParallelOffsetInMs.Visible = true;
                 picParallel.Image = Resources.Parallel;
-            }
-            else
-            {
+            } else {
                 picIgnoreDelay.Visible = true;
                 nudParallelOffsetInMs.Visible = false;
                 picParallel.Image = Resources.NotParallel;
-                BackColor = (index == 1 && _logEntry.Parent is UserAction && _logEntry.Parent.Count != 0)
-                                ? Color.LightGray
-                                : SystemColors.Control;
+                if (index == 1 && _logEntry.Parent is UserAction && _logEntry.Parent.Count != 0)
+                    BackColor = Color.LightGray;
+                else
+                    BackColor = (((float)index % 2) == 0) ? SystemColors.ControlLight : SystemColors.Control;
             }
+#else
+            BackColor = (((float)index % 2) == 0) ? SystemColors.ControlLight : SystemColors.Control;
 #endif
         }
 
-        protected override void Select(bool directed, bool forward)
-        {
+        protected override void Select(bool directed, bool forward) {
             base.Select(directed, forward);
         }
 
