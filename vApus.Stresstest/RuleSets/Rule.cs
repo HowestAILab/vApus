4c6852ae0255b39c5920103e4864b75c260bcda6
Dieter Vandroemme <dieter@sizingservers.be>
2013-04-04 12:01:17 +0200

    #410

--- vApus.Stresstest/RuleSets/Rule.cs
+++ vApus.Stresstest/RuleSets/Rule.cs
@@ -12,32 +12,41 @@ using System.Text.RegularExpressions;
 using System.Windows.Forms;
 using vApus.SolutionTree;
 
-namespace vApus.Stresstest
-{
-    [ContextMenu(new[] {"Activate_Click", "Remove_Click", "Copy_Click", "Cut_Click", "Duplicate_Click"},
-        new[] {"Edit", "Remove", "Copy", "Cut", "Duplicate"})]
-    [Hotkeys(new[] {"Activate_Click", "Remove_Click", "Copy_Click", "Cut_Click", "Duplicate_Click"},
-        new[] {Keys.Enter, Keys.Delete, (Keys.Control | Keys.C), (Keys.Control | Keys.X), (Keys.Control | Keys.D)})]
+namespace vApus.Stresstest {
+    [ContextMenu(new[] { "Activate_Click", "Remove_Click", "Copy_Click", "Cut_Click", "Duplicate_Click" },
+        new[] { "Edit", "Remove", "Copy", "Cut", "Duplicate" })]
+    [Hotkeys(new[] { "Activate_Click", "Remove_Click", "Copy_Click", "Cut_Click", "Duplicate_Click" },
+        new[] { Keys.Enter, Keys.Delete, (Keys.Control | Keys.C), (Keys.Control | Keys.X), (Keys.Control | Keys.D) })]
     [Serializable]
-    public class Rule : LabeledBaseItem
-    {
+    public class Rule : LabeledBaseItem {
         #region Enum
 
         [Serializable]
-        public enum ValueTypes
-        {
-            [Description("String")] stringType,
-            [Description("Single character")] charType,
-            [Description("16-bit integer")] shortType,
-            [Description("32-bit integer")] intType,
-            [Description("64-bit integer")] longType,
-            [Description("Unsigned 16-bit integer")] ushortType,
-            [Description("Unsigned 32-bit integer")] uintType,
-            [Description("Unsigned 64-bit integer")] ulongType,
-            [Description("32-bit floating point")] floatType,
-            [Description("Numeric")] doubleType,
-            [Description("Decimal")] decimalType,
-            [Description("Boolean")] boolType
+        public enum ValueTypes {
+            [Description("String")]
+            stringType,
+            [Description("Single character")]
+            charType,
+            [Description("16-bit integer")]
+            shortType,
+            [Description("32-bit integer")]
+            intType,
+            [Description("64-bit integer")]
+            longType,
+            [Description("Unsigned 16-bit integer")]
+            ushortType,
+            [Description("Unsigned 32-bit integer")]
+            uintType,
+            [Description("Unsigned 64-bit integer")]
+            ulongType,
+            [Description("32-bit floating point")]
+            floatType,
+            [Description("Numeric")]
+            doubleType,
+            [Description("Decimal")]
+            decimalType,
+            [Description("Boolean")]
+            boolType
         }
 
         #endregion
@@ -57,24 +66,21 @@ namespace vApus.Stresstest
         [SavableCloneable, PropertyControl(0)]
         [Description("The actual rule. [http://www.google.com/search?q=regular+expressions]"),
          DisplayName("Regular Expression")]
-        public string RegExp
-        {
+        public string RegExp {
             get { return _regExp; }
             set { _regExp = value; }
         }
 
         [SavableCloneable, PropertyControl(1)]
         [Description("Ignore case for the regular expression."), DisplayName("Ignore Case")]
-        public bool IgnoreCase
-        {
+        public bool IgnoreCase {
             get { return _ignoreCase; }
             set { _ignoreCase = value; }
         }
 
         [SavableCloneable, PropertyControl(2)]
         [Description("The input must equal or castable/parsable to the selected type."), DisplayName("Value Type")]
-        public ValueTypes ValueType
-        {
+        public ValueTypes ValueType {
             get { return _valueType; }
             set { _valueType = value; }
         }
@@ -83,16 +89,14 @@ namespace vApus.Stresstest
         [Description(
             "If implemented, textual input will be displayed using the password character. Where this is implemented encryption is always used."
             ), DisplayName("Use Password Character")]
-        public bool UsePasswordChar
-        {
+        public bool UsePasswordChar {
             get { return _usePasswordChar; }
             set { _usePasswordChar = value; }
         }
 
         [SavableCloneable, PropertyControl(4)]
         [Description("Describes this rule.")]
-        public virtual string Description
-        {
+        public virtual string Description {
             get { return _description; }
             set { _description = value; }
         }
@@ -105,16 +109,12 @@ namespace vApus.Stresstest
 
         #region Functions
 
-        internal LexicalResult TryLexicalAnaysis(string input, Parameters parameters, out ASTNode output)
-        {
+        internal LexicalResult TryLexicalAnaysis(string input, Parameters parameters, out ASTNode output) {
             output = new ASTNode(parameters);
             output.Value = input;
-            try
-            {
+            try {
                 CastOrParseInputToType(input, _valueType);
-            }
-            catch
-            {
+            } catch {
                 output.Error = string.Format("The input could not be parsed to {0}.", _valueType);
                 return LexicalResult.Error;
             }
@@ -127,75 +127,68 @@ namespace vApus.Stresstest
             return LexicalResult.Error;
         }
 
-        private object CastOrParseInputToType(object input, ValueTypes valueType)
-        {
-            try
-            {
-                switch (valueType)
-                {
+        private object CastOrParseInputToType(object input, ValueTypes valueType) {
+            try {
+                switch (valueType) {
                     case ValueTypes.boolType:
-                        return (input is bool) ? (bool) input : bool.Parse(input.ToString());
+                        return (input is bool) ? (bool)input : bool.Parse(input.ToString());
                     case ValueTypes.charType:
-                        return (input is char) ? (char) input : char.Parse(input.ToString());
+                        return (input is char) ? (char)input : char.Parse(input.ToString());
                     case ValueTypes.decimalType:
-                        return (input is decimal) ? (decimal) input : decimal.Parse(input.ToString());
+                        return (input is decimal) ? (decimal)input : decimal.Parse(input.ToString());
                     case ValueTypes.doubleType:
-                        return (input is double) ? (double) input : double.Parse(input.ToString());
+                        return (input is double) ? (double)input : double.Parse(input.ToString());
                     case ValueTypes.floatType:
-                        return (input is float) ? (float) input : float.Parse(input.ToString());
+                        return (input is float) ? (float)input : float.Parse(input.ToString());
                     case ValueTypes.intType:
-                        return (input is int) ? (int) input : int.Parse(input.ToString());
+                        return (input is int) ? (int)input : int.Parse(input.ToString());
                     case ValueTypes.longType:
-                        return (input is long) ? (long) input : long.Parse(input.ToString());
+                        return (input is long) ? (long)input : long.Parse(input.ToString());
                     case ValueTypes.shortType:
-                        return (input is short) ? (short) input : short.Parse(input.ToString());
+                        return (input is short) ? (short)input : short.Parse(input.ToString());
                     case ValueTypes.stringType:
                         return input.ToString();
                     case ValueTypes.uintType:
-                        return (input is uint) ? (uint) input : uint.Parse(input.ToString());
+                        return (input is uint) ? (uint)input : uint.Parse(input.ToString());
                     case ValueTypes.ulongType:
-                        return (input is ulong) ? (ulong) input : ulong.Parse(input.ToString());
+                        return (input is ulong) ? (ulong)input : ulong.Parse(input.ToString());
                     case ValueTypes.ushortType:
-                        return (input is ushort) ? (ushort) input : ushort.Parse(input.ToString());
+                        return (input is ushort) ? (ushort)input : ushort.Parse(input.ToString());
                     default:
                         return input;
                 }
-            }
-            catch
-            {
+            } catch {
                 throw new Exception(
                     "Please make sure the \"Value to Check Against\" is castable/parsable to the selected \"Value Type\".");
             }
         }
 
-        public static Type GetType(ValueTypes valueType)
-        {
-            switch (valueType)
-            {
+        public static Type GetType(ValueTypes valueType) {
+            switch (valueType) {
                 case ValueTypes.stringType:
-                    return typeof (string);
+                    return typeof(string);
                 case ValueTypes.charType:
-                    return typeof (char);
+                    return typeof(char);
                 case ValueTypes.shortType:
-                    return typeof (short);
+                    return typeof(short);
                 case ValueTypes.intType:
-                    return typeof (int);
+                    return typeof(int);
                 case ValueTypes.longType:
-                    return typeof (long);
+                    return typeof(long);
                 case ValueTypes.ushortType:
-                    return typeof (ushort);
+                    return typeof(ushort);
                 case ValueTypes.uintType:
-                    return typeof (uint);
+                    return typeof(uint);
                 case ValueTypes.ulongType:
-                    return typeof (ulong);
+                    return typeof(ulong);
                 case ValueTypes.floatType:
-                    return typeof (float);
+                    return typeof(float);
                 case ValueTypes.doubleType:
-                    return typeof (double);
+                    return typeof(double);
                 case ValueTypes.decimalType:
-                    return typeof (decimal);
+                    return typeof(decimal);
                 case ValueTypes.boolType:
-                    return typeof (bool);
+                    return typeof(bool);
                 default:
                     return null;
             }
--- vApus.Util/CompilerUnit.cs
+++ vApus.Util/CompilerUnit.cs
@@ -15,19 +15,16 @@ using System.Threading;
 using System.Windows.Forms;
 using Microsoft.CSharp;
 
-namespace vApus.Util
-{
+namespace vApus.Util {
     /// <summary>
     ///     A C# .net v4.0 compiler unit.
     ///     For references there is linked in Application.StartupPath or in the folders given in ReferenceResolver.ini if the Use property of ReferenceResolver is set to true.
     /// </summary>
-    public class CompilerUnit
-    {
+    public class CompilerUnit {
         private readonly List<TempFileCollection> _tempFiles = new List<TempFileCollection>();
         private readonly string _tempFilesDirectory = Path.Combine(Application.StartupPath, "ConnectionProxyTempFiles");
 
-        public CompilerUnit()
-        {
+        public CompilerUnit() {
             Application.ApplicationExit += Application_ApplicationExit;
         }
 
@@ -40,8 +37,7 @@ namespace vApus.Util
         /// <param name="debug">For compiling with an attached debugger. Compile warnings are threated as errros.</param>
         /// <param name="compilerResults"></param>
         /// <returns></returns>
-        public Assembly Compile(string source, bool debug, out CompilerResults compilerResults)
-        {
+        public Assembly Compile(string source, bool debug, out CompilerResults compilerResults) {
             return Compile(source, null, debug, out compilerResults);
         }
 
@@ -54,8 +50,7 @@ namespace vApus.Util
         /// <param name="debug">For compiling with an attached debugger. Compile warnings are threated as errros.</param>
         /// <param name="compilerResults"></param>
         /// <returns></returns>
-        public Assembly Compile(string[] sources, bool debug, out CompilerResults compilerResults)
-        {
+        public Assembly Compile(string[] sources, bool debug, out CompilerResults compilerResults) {
             return Compile(sources, null, debug, out compilerResults);
         }
 
@@ -69,9 +64,8 @@ namespace vApus.Util
         /// <param name="debug">For compiling with an attached debugger. Compile warnings are threated as errros.</param>
         /// <param name="compilerResults"></param>
         /// <returns></returns>
-        public Assembly Compile(string source, string outputAssembly, bool debug, out CompilerResults compilerResults)
-        {
-            return Compile(new[] {source}, outputAssembly, debug, out compilerResults);
+        public Assembly Compile(string source, string outputAssembly, bool debug, out CompilerResults compilerResults) {
+            return Compile(new[] { source }, outputAssembly, debug, out compilerResults);
         }
 
         /// <summary>
@@ -84,8 +78,7 @@ namespace vApus.Util
         /// <param name="debug">For compiling with an attached debugger. Compile warnings are threated as errros.</param>
         /// <param name="compilerResults"></param>
         /// <returns></returns>
-        public Assembly Compile(string[] sources, string outputAssembly, bool debug, out CompilerResults compilerResults)
-        {
+        public Assembly Compile(string[] sources, string outputAssembly, bool debug, out CompilerResults compilerResults) {
             if (sources.Length == 0)
                 throw new Exception("Nothing to compile.");
 
@@ -98,16 +91,14 @@ namespace vApus.Util
             if (outputAssembly != null)
                 compilerParameters.OutputAssembly = outputAssembly;
 
-            if (debug)
-            {
+            if (debug) {
                 //Will temp output the assembly while it is compiled in memory = no locking
                 if (!Directory.Exists(_tempFilesDirectory))
                     Directory.CreateDirectory(_tempFilesDirectory);
 
                 string readme = Path.Combine(_tempFilesDirectory, "README.TXT");
                 if (!File.Exists(readme))
-                    using (var sw = new StreamWriter(readme))
-                    {
+                    using (var sw = new StreamWriter(readme)) {
                         sw.Write(
                             "These files are compiled connection proxies used for stresstesting, this folder can be removed safely.");
                         sw.Flush();
@@ -116,9 +107,7 @@ namespace vApus.Util
                 compilerParameters.IncludeDebugInformation = true;
                 compilerParameters.TempFiles = new TempFileCollection(_tempFilesDirectory, true);
                 _tempFiles.Add(compilerParameters.TempFiles);
-            }
-            else
-            {
+            } else {
                 compilerParameters.GenerateInMemory = true;
                 compilerParameters.IncludeDebugInformation = false;
                 //  compilerParameters.TempFiles = new TempFileCollection(_tempFilesDirectory, false);
@@ -134,35 +123,26 @@ namespace vApus.Util
             compilerResults = null;
 
             for (int i = 1; i != 4; i++)
-                try
-                {
+                try {
                     compilerResults = compiler.CompileAssemblyFromSource(compilerParameters, sources);
-                    if (compilerResults.Errors.HasErrors && i == 3 || !compilerResults.Errors.HasErrors)
-                    {
+                    if (compilerResults.Errors.HasErrors && i == 3 || !compilerResults.Errors.HasErrors) {
                         break;
-                    }
-                    else
-                    {
+                    } else {
                         if (debug)
                             break;
 
                         compilerResults.Errors.Clear();
-                        Thread.Sleep(1000*i);
+                        Thread.Sleep(1000 * i);
                     }
-                }
-                catch
-                {
+                } catch {
                 }
 
             if (!debug)
                 DeleteTempFiles();
 
-            try
-            {
+            try {
                 return compilerResults.CompiledAssembly;
-            }
-            catch
-            {
+            } catch {
                 return null;
             }
         }
@@ -174,25 +154,19 @@ namespace vApus.Util
         /// </summary>
         /// <param name="compilerParamaters"></param>
         /// <param name="fileName"></param>
-        private void AddAllReferencedAssemblies(string source, CompilerParameters compilerParamaters)
-        {
+        private void AddAllReferencedAssemblies(string source, CompilerParameters compilerParamaters) {
             bool found = false;
             source = source.Replace(Environment.NewLine, "\n").Replace('\r', '\n');
             foreach (string line in source.Split('\n'))
-                if (line.StartsWith("// dllreferences:"))
-                {
-                    string[] dllReferences = line.Split(':')[1].Split(new[] {";"}, StringSplitOptions.RemoveEmptyEntries);
-                    foreach (string dllReference in dllReferences)
-                    {
+                if (line.StartsWith("// dllreferences:")) {
+                    string[] dllReferences = line.Split(':')[1].Split(new[] { ";" }, StringSplitOptions.RemoveEmptyEntries);
+                    foreach (string dllReference in dllReferences) {
                         List<string> matches = GetMatches(dllReference);
-                        if (matches.Count != 0)
-                        {
+                        if (matches.Count != 0) {
                             bool matchFound = false;
-                            foreach (string match in matches)
-                            {
+                            foreach (string match in matches) {
                                 string[] splitMatch = match.Split('\\');
-                                if (splitMatch[splitMatch.Length - 1] == dllReference)
-                                {
+                                if (splitMatch[splitMatch.Length - 1] == dllReference) {
                                     compilerParamaters.ReferencedAssemblies.Add(match);
                                     matchFound = true;
                                     break;
@@ -200,22 +174,17 @@ namespace vApus.Util
                             }
                             if (!matchFound)
                                 compilerParamaters.ReferencedAssemblies.Add(dllReference);
-                        }
-                        else
-                        {
+                        } else {
                             compilerParamaters.ReferencedAssemblies.Add(dllReference);
                         }
                     }
                     found = true;
-                }
-                else if (found)
-                {
+                } else if (found) {
                     break;
                 }
         }
 
-        private List<string> GetMatches(string dllReference)
-        {
+        private List<string> GetMatches(string dllReference) {
             var matches =
                 new List<string>(Directory.GetFiles(Application.StartupPath, dllReference, SearchOption.AllDirectories));
             //if (ReferenceResolver.Use)
@@ -237,40 +206,28 @@ namespace vApus.Util
         /// <summary>
         ///     Delete the tempFiles generated on compiling when debugging, if possible.
         /// </summary>
-        public void DeleteTempFiles()
-        {
-            foreach (TempFileCollection tempFiles in _tempFiles)
-            {
+        public void DeleteTempFiles() {
+            foreach (TempFileCollection tempFiles in _tempFiles) {
                 tempFiles.KeepFiles = false;
-                try
-                {
+                try {
                     tempFiles.Delete();
-                }
-                catch
-                {
+                } catch {
                 }
             }
             _tempFiles.Clear();
 
             if (Directory.Exists(_tempFilesDirectory))
-                try
-                {
+                try {
                     Directory.Delete(_tempFilesDirectory, true);
-                }
-                catch
-                {
+                } catch {
                 }
         }
 
-        private void Application_ApplicationExit(object sender, EventArgs e)
-        {
+        private void Application_ApplicationExit(object sender, EventArgs e) {
             if (Directory.Exists(_tempFilesDirectory))
-                try
-                {
+                try {
                     Directory.Delete(_tempFilesDirectory, true);
-                }
-                catch
-                {
+                } catch {
                 }
         }
     }
