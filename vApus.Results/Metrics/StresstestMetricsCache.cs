6f2e4910cd467a98806e32c737c3384b522af4aa
Dieter Vandroemme <dieter@sizingservers.be>
2014-05-12 13:51:33 +0200

    #669 again

--- vApus.Results/Metrics/StresstestMetricsCache.cs
+++ vApus.Results/Metrics/StresstestMetricsCache.cs
@@ -126,7 +126,7 @@ namespace vApus.Results {
             foreach (MetricsCacheObject o in _cache)
                 if (o.Metrics.Run != 0) {
                     if (o.Result != null)
-                        if ((_calculateSimplifiedMetrics && !_allowSimplifiedMetrics) || (o.Metrics.LogEntries == 0 || o.Metrics.LogEntriesProcessed != o.Metrics.LogEntries))
+                        if (!_allowSimplifiedMetrics || o.Metrics.LogEntries == 0 || o.Metrics.LogEntriesProcessed != o.Metrics.LogEntries)
                             o.Metrics = StresstestMetricsHelper.GetMetrics(o.Result as RunResult, ref _calculateSimplifiedMetrics, _allowSimplifiedMetrics, false, _allowSimplifiedMetrics);
                     metrics.Add(o.Metrics);
                 }
