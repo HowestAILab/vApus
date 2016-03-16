ceded18dd0ebadaa177c2d78af58e96c2512944b
Dieter Vandroemme <dieter@sizingservers.be>
2016-03-16 15:00:34 +0100

    Added a value store, gui a bit rough around the edges.

---
+++ vApus.StressTest/ValueStore/ValueStore.cs
@@ -0,0 +1,94 @@
+﻿/*
+ * Copyright 2016 (c) Sizing Servers Lab
+ * University College of West-Flanders, Department GKG
+ * 
+ * Author(s):
+ *    Dieter Vandroemme
+ */
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.Serialization;
+using System.Windows.Forms;
+using vApus.SolutionTree;
+using vApus.Util;
+
+namespace vApus.StressTest {
+    /// <summary>
+    /// Stores values that can be used in cps.
+    /// </summary>
+    [ContextMenu(new[] { "Add_Click", "Import_Click", "Clear_Click", "Paste_Click" },
+     new[] { "Add value", "Import values(s)", "Clear", "Paste" })]
+    [Hotkeys(new[] { "Add_Click", "Paste_Click" }, new[] { Keys.Insert, (Keys.Control | Keys.V) })]
+    [DisplayName("Value store")]
+    [Serializable]
+    public class ValueStore : BaseItem, ISerializable {
+        private static ValueStore _valueStore;
+        /// <summary>
+        /// Faster than a lock around this.
+        /// </summary>
+        private static ConcurrentDictionary<string, ValueStoreValue> _valuesForCPs = new ConcurrentDictionary<string, ValueStoreValue>();
+
+        #region Constructors
+        /// <summary>
+        /// Stores values that can be used in cps.
+        /// </summary>
+        public ValueStore() {
+            AddAsDefaultItem(new ValueStoreValue() { Label = "publish", Publish = true });
+        }
+
+        public ValueStore(SerializationInfo info, StreamingContext ctxt) {
+            _valueStore = this;
+            SerializationReader sr;
+            using (sr = SerializationReader.GetReader(info)) {
+                ShowInGui = false;
+                AddRangeWithoutInvokingEvent(sr.ReadCollection<BaseItem>(new List<BaseItem>()));
+            }
+            sr = null;
+        }
+        #endregion
+
+        #region Functions
+        private void Add_Click(object sender, EventArgs e) { Add(new ValueStoreValue()); }
+        public void GetObjectData(SerializationInfo info, StreamingContext context) {
+            SerializationWriter sw;
+            using (sw = SerializationWriter.GetWriter()) {
+                sw.Write(this);
+                sw.AddToInfo(info);
+            }
+            sw = null;
+        }
+
+        /// <summary>
+        /// Always do this before a test.
+        /// </summary>
+        public void InitForTest(string resultSetId, string test) {
+            _valuesForCPs.Clear();
+
+            foreach (ValueStoreValue v in this) {
+                v.ResultSetId = resultSetId;
+                v.Test = test;
+
+                if (v.ClearBeforeTest) v.ClearValues();
+
+                if (!string.IsNullOrWhiteSpace(v.Label))
+                    _valuesForCPs.TryAdd(v.Label, v);
+            }
+        }
+
+        /// <summary>
+        /// Used in CP.
+        /// </summary>
+        /// <param name="label"></param>
+        /// <returns></returns>
+        public static ValueStoreValue GetValueStoreValue(string label) {
+            ValueStoreValue valueStoreValue;
+            if (!_valuesForCPs.TryGetValue(label, out valueStoreValue))
+                throw new Exception("Value store value with label " + label + " not found.");
+            return valueStoreValue;
+        }
+
+        #endregion
+    }
+}
---
+++ vApus.Stresstest/ValueStore/ValueStoreValueView.cs
@@ -0,0 +1,153 @@
+﻿/*
+ * Copyright 2016 (c) Sizing Servers Lab
+ * University College of West-Flanders, Department GKG
+ * 
+ * Author(s):
+ *    Dieter Vandroemme
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using vApus.SolutionTree;
+
+namespace vApus.StressTest {
+    public partial class ValueStoreValueView : BaseSolutionComponentView {
+        private ValueStoreValue _valueStoreValue;
+
+        private ValueStoreValue.ValueStoreValueTypes _previousType;
+
+        /// <summary>
+        ///     Designer time constructor.
+        /// </summary>
+        public ValueStoreValueView() {
+            InitializeComponent();
+        }
+        public ValueStoreValueView(SolutionComponent solutionComponent)
+            : base(solutionComponent) {
+            _valueStoreValue = solutionComponent as ValueStoreValue;
+            _previousType = _valueStoreValue.Type;
+
+            InitializeComponent();
+
+            if (IsHandleCreated)
+                SetGui();
+            else
+                HandleCreated += _HandleCreated;
+        }
+
+        private void _HandleCreated(object sender, EventArgs e) { SetGui(); }
+        private void SetGui() {
+            Text = SolutionComponent.ToString();
+            solutionComponentPropertyPanel.SolutionComponent = SolutionComponent;
+
+            SolutionComponent.SolutionComponentChanged += SolutionComponent_SolutionComponentChanged;
+
+            btnRefresh.PerformClick();
+        }
+
+        private void SolutionComponent_SolutionComponentChanged(object sender, SolutionComponentChangedEventArgs e) {
+            if (sender == _valueStoreValue) {
+                if (_valueStoreValue.Type != _previousType) {
+                    _previousType = _valueStoreValue.Type;
+                    solutionComponentPropertyPanel.SetValues(true, solutionComponentPropertyPanel.Values);
+                }
+                btnRefresh.PerformClick();
+            }
+        }
+
+        public override void Refresh() {
+            base.Refresh();
+            SetGui();
+            solutionComponentPropertyPanel.Refresh();
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e) {
+            var sb = new StringBuilder();
+            var values = _valueStoreValue.Values.ToArray();
+
+            if (values.Length == 0) {
+                sb.AppendLine("No value set, falling back to the default value \"" + _valueStoreValue.DefaultValue + "\" (without quotes).");
+
+                if (_valueStoreValue.Publish) {
+                    sb.AppendLine();
+                    sb.AppendLine("Each time the value is set it will be published, if publishing is enabled (see Tools > Options... > Publish values).");
+                }
+                sb.Append(@"
+
+Usage example in connection proxy code:
+
+  // Reference the ValueStoreValue in a field. Use the label of the ValueStoreValue as variable name.
+  ValueStoreValue _myValue = ValueStore.GetValueStoreValue(Value store value label e.g. " + "\"myValue\"" + @");
+
+  var defaultValue = _myValue.DefaultValue;
+
+  // Respect the chosen type, otherwise setting a value will fail.
+  // If the value must be unique for each connection, this call will only work in SendAndReceive(...) and sub functions.
+  // If not, this will work in every function (e.g. TestConnection(out error)).
+  _myValue.Set(" + "\"foobar\"" + @"); 
+
+  // You can get the names of the threads who set a value.
+  // 'shared' (without quotes) is returned when not unique for each connection.
+  string[] owners = _myValue.GetOwners();
+
+  // Get the 'shared' value or the value set for the current connection or for a chosen one (owner).
+  // This value can be used to replace a token (a piece of text for instance " + "\"{myValue}\"" + @") in the request, coming through SendAndReceive(...).
+  string foo = _myValue.Get<string>();
+  string bar = _myValue.Get<string>(owner e.g. " + "\"vApus Thread Pool Thread #1\"" + ");"
+                    );
+            }
+            else {
+
+                //Sort stuff.
+                string prefix = "vApus Thread Pool Thread #";
+                var dic = new SortedDictionary<string, object>(KeyComparer.GetInstance());
+                KeyValuePair<string, object> kvp;
+
+                for (int i = 0; i != values.Length; i++) {
+                    kvp = values[i];
+                    dic.Add(kvp.Key.Substring(prefix.Length), kvp.Value);
+                }
+                dic.CopyTo(values, 0);
+
+                //Print stuff.
+                sb.AppendLine(prefix);
+
+                for (int i = 0; i != values.Length - 1; i++) {
+                    kvp = values[i];
+                    sb.Append("  ");
+                    sb.Append(kvp.Key);
+                    sb.Append(":\t");
+                    sb.AppendLine(kvp.Value.ToString());
+                }
+
+                kvp = values.Last();
+                sb.Append("  ");
+                sb.Append(kvp.Key);
+                sb.Append(":\t");
+                sb.Append(kvp.Value.ToString());
+            }
+
+            fctb.Text = sb.ToString().Trim();
+        }
+
+        private class KeyComparer : IComparer<string> {
+            private static KeyComparer _instance;
+
+            public static KeyComparer GetInstance() {
+                if (_instance == null) _instance = new KeyComparer();
+                return _instance;
+            }
+
+            private KeyComparer() { }
+
+            public int Compare(string x, string y) {
+
+                int i = int.Parse(x);
+                int j = int.Parse(y);
+
+                return i.CompareTo(j);
+            }
+        }
+    }
+}
