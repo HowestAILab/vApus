52c83f3aa3a073afbeefd3137e5886d5941bcd96
Dieter Vandroemme <dieter@sizingservers.be>
2016-11-16 08:43:37 +0100

    Updated icones once more

--- vApus.UpdateTool/Update.designer.cs
+++ vApus.UpdateTool/Update.designer.cs
@@ -47,9 +47,10 @@
             this.lvwUpdate.Dock = System.Windows.Forms.DockStyle.Fill;
             this.lvwUpdate.FullRowSelect = true;
             this.lvwUpdate.Location = new System.Drawing.Point(0, 0);
+            this.lvwUpdate.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.lvwUpdate.MultiSelect = false;
             this.lvwUpdate.Name = "lvwUpdate";
-            this.lvwUpdate.Size = new System.Drawing.Size(836, 536);
+            this.lvwUpdate.Size = new System.Drawing.Size(1115, 660);
             this.lvwUpdate.TabIndex = 0;
             this.lvwUpdate.UseCompatibleStateImageBehavior = false;
             this.lvwUpdate.View = System.Windows.Forms.View.Details;
@@ -76,13 +77,14 @@
             // 
             // Update
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(836, 536);
+            this.ClientSize = new System.Drawing.Size(1115, 660);
             this.Controls.Add(this.lvwUpdate);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MinimizeBox = false;
             this.Name = "Update";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
