bbe7fca56bf42a479bf865d45d8e1a8ec8cbe964
Dieter Vandroemme <dieter@sizingservers.be>
2017-09-26 16:51:59 +0200

    same-spaced font for hardware config dialog. ValueStoreView could not show the value for a shared valuestorevalue.

--- vApus.Monitor/HardwareConfigurationDialog.designer.cs
+++ vApus.Monitor/HardwareConfigurationDialog.designer.cs
@@ -35,10 +35,12 @@ namespace vApus.Monitor {
             | System.Windows.Forms.AnchorStyles.Right)));
             this.rtxt.BackColor = System.Drawing.Color.White;
             this.rtxt.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.rtxt.Font = new System.Drawing.Font("Consolas", 10.15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.rtxt.Location = new System.Drawing.Point(0, 0);
+            this.rtxt.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.rtxt.Name = "rtxt";
             this.rtxt.ReadOnly = true;
-            this.rtxt.Size = new System.Drawing.Size(592, 443);
+            this.rtxt.Size = new System.Drawing.Size(789, 545);
             this.rtxt.TabIndex = 0;
             this.rtxt.Text = "";
             this.rtxt.WordWrap = false;
@@ -49,9 +51,10 @@ namespace vApus.Monitor {
             this.btnSave.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSave.Location = new System.Drawing.Point(0, 443);
+            this.btnSave.Location = new System.Drawing.Point(0, 546);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnSave.Name = "btnSave";
-            this.btnSave.Size = new System.Drawing.Size(592, 23);
+            this.btnSave.Size = new System.Drawing.Size(789, 28);
             this.btnSave.TabIndex = 3;
             this.btnSave.Text = "Save...";
             this.btnSave.UseVisualStyleBackColor = false;
@@ -65,14 +68,15 @@ namespace vApus.Monitor {
             // 
             // HardwareConfigurationDialog
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(592, 466);
+            this.ClientSize = new System.Drawing.Size(789, 574);
             this.Controls.Add(this.rtxt);
             this.Controls.Add(this.btnSave);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MinimizeBox = false;
-            this.MinimumSize = new System.Drawing.Size(600, 500);
+            this.MinimumSize = new System.Drawing.Size(794, 605);
             this.Name = "HardwareConfigurationDialog";
             this.ShowIcon = false;
             this.ShowInTaskbar = false;
